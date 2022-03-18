357b98073e7aae142be1ea7e9fd5055b46791ec0
wangyutian <wangyutian@jilinxiangyun.com>
2022-03-18 19:02:26 +0800

    commit

--- src/main/java/com/atguigu/apitest/source/SourceTest3_Kafka.java
+++ src/main/java/com/atguigu/apitest/source/SourceTest3_Kafka.java
@@ -1,10 +1,18 @@
 package com.atguigu.apitest.source;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -13,19 +21,27 @@ import java.util.Properties;
  * @date 2022/3/17
  */
 public class SourceTest3_Kafka {
-    public static void main(String[] args) throws Exception{
+    public static <OUT> void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
 
+        // kafka读取数据
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("group.id", "consumer-group");
         properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.setProperty("auto.offset.reset", "latest");
 
-        // 从文件读取数据
-        DataStream<String> dataStream = env.addSource( new FlinkKafkaConsumer011<String>("sensor", new SimpleStringSchema(), properties));
+        DataStream<String> dataStream = env.addSource(new FlinkKafkaConsumer<>("sensor", new SimpleStringSchema(), properties));
+
+        // 使用KafkaSource读取数据
+//        KafkaSource<String> source = KafkaSource.<String>builder()
+//                .setBootstrapServers("localhost:9092")
+//                .setTopics("test2")
+//                .setGroupId("consumer-group")
+//                .setValueOnlyDeserializer(new SimpleStringSchema())
+//                .build();
+//        DataStreamSource<String> dataStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+
 
         // 打印输出
         dataStream.print();

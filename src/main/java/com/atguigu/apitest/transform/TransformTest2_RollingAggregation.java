a8d4bfcca34273e36dcbc257165f3cbe15cd8126
wangyutian <wangyutian@jilinxiangyun.com>
2022-03-19 17:05:45 +0800

    commit

--- src/main/java/com/atguigu/apitest/transform/TransformTest2_RollingAggregation.java
+++ src/main/java/com/atguigu/apitest/transform/TransformTest2_RollingAggregation.java
@@ -1,74 +1,42 @@
-package com.atguigu.apitest.transform;/**
- * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved
- * <p>
- * Project: FlinkTutorial
- * Package: com.atguigu.apitest.transform
- * Version: 1.0
- * <p>
- * Created by wushengran on 2020/11/7 15:09
- */
+package com.atguigu.apitest.transform;
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import sun.awt.SunHints;
-
-import javax.xml.crypto.Data;
 
 /**
- * @ClassName: TransformTest2_RollingAggregation
- * @Description:
- * @Author: wushengran on 2020/11/7 15:09
- * @Version: 1.0
+ * @author wangyutian
+ * @version 1.0
+ * @date 2022/3/18
  */
 public class TransformTest2_RollingAggregation {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(4);
+        env.setParallelism(1);
 
         // 从文件读取数据
-        DataStream<String> inputStream = env.readTextFile("D:\\Projects\\BigData\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
+        DataStream<String> inputStream = env.readTextFile("D:\\code\\flink\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
 
-        // 转换成SensorReading类型
-//        DataStream<SensorReading> dataStream = inputStream.map(new MapFunction<String, SensorReading>() {
-//            @Override
-//            public SensorReading map(String value) throws Exception {
-//                String[] fields = value.split(",");
-//                return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
-//            }
-//        });
-
-        DataStream<SensorReading> dataStream = inputStream.map( line -> {
-            String[] fields = line.split(",");
-            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
-        } );
-
-        // 分组
-        KeyedStream<SensorReading, Tuple> keyedStream = dataStream.keyBy("id");
-        KeyedStream<SensorReading, String> keyedStream1 = dataStream.keyBy(data -> data.getId());
-
-        DataStream<Long> dataStream1 = env.fromElements(1L, 34L, 4L, 657L, 23L);
-        KeyedStream<Long, Integer> keyedStream2 = dataStream1.keyBy(new KeySelector<Long, Integer>() {
+        // input数据转换成pojo
+        SingleOutputStreamOperator<SensorReading> dataStream = inputStream.map(new MapFunction<String, SensorReading>() {
             @Override
-            public Integer getKey(Long value) throws Exception {
-                return value.intValue() % 2;
+            public SensorReading map(String s) {
+                String[] fields = s.split(",");
+                return new SensorReading(fields[0], Long.parseLong(fields[1]), Double.parseDouble(fields[2]));
             }
         });
 
-        //        KeyedStream<SensorReading, String> keyedStream1 = dataStream.keyBy(SensorReading::getId);
+        // 分组
+        KeyedStream<SensorReading, String> keyedStream = dataStream.keyBy(SensorReading::getId);
 
-        // 滚动聚合，取当前最大的温度值
-        DataStream<SensorReading> resultStream = keyedStream.maxBy("temperature");
+        // 滚动聚合，获取当前最大温度
+        SingleOutputStreamOperator<SensorReading> result = keyedStream.maxBy("temperature");
 
-        resultStream.print("result");
+        result.print();
 
-        keyedStream1.print("key1");
-        keyedStream2.sum(0).print("key2");
         env.execute();
     }
 }

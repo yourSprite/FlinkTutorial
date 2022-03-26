96bf9d194af231386d0af3d9cab063d529bd47d7
wangyutian <wangyutian@jilinxiangyun.com>
2022-03-26 22:39:36 +0800

    commit

--- src/main/java/com/atguigu/apitest/window/WindowTest3_EventTimeWindow.java
+++ src/main/java/com/atguigu/apitest/window/WindowTest3_EventTimeWindow.java
@@ -1,34 +1,37 @@
-package com.atguigu.apitest.window;/**
- * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved
- * <p>
- * Project: FlinkTutorial
- * Package: com.atguigu.apitest.window
- * Version: 1.0
- * <p>
- * Created by wushengran on 2020/11/10 9:33
- */
+package com.atguigu.apitest.window;
 
 import com.atguigu.apitest.beans.SensorReading;
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkGenerator;
+import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.Trigger;
+import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.util.OutputTag;
 
+import java.time.Duration;
+import java.util.Collection;
+
 /**
- * @ClassName: WindowTest3_EventTimeWindow
- * @Description:
- * @Author: wushengran on 2020/11/10 9:33
- * @Version: 1.0
+ * @author wangyutian
+ * @version 1.0
+ * @date 2022/3/26
  */
 public class WindowTest3_EventTimeWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//        env.setParallelism(1);
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         env.getConfig().setAutoWatermarkInterval(100);
 
         // socket文本流
@@ -36,30 +39,27 @@ public class WindowTest3_EventTimeWindow {
 
         // 转换成SensorReading类型，分配时间戳和watermark
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
-            String[] fields = line.split(",");
-            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
-        })
+                    String[] fields = line.split(",");
+                    return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+                })
                 // 升序数据设置事件时间和watermark
-//                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorReading>() {
-//                    @Override
-//                    public long extractAscendingTimestamp(SensorReading element) {
-//                        return element.getTimestamp() * 1000L;
-//                    }
-//                })
-                // 乱序数据设置时间戳和watermark
-                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<SensorReading>(Time.seconds(2)) {
-                    @Override
-                    public long extractTimestamp(SensorReading element) {
-                        return element.getTimestamp() * 1000L;
-                    }
-                });
+//                .assignTimestampsAndWatermarks(WatermarkStrategy.<SensorReading>forMonotonousTimestamps()
+//                        .withTimestampAssigner((element, recordTimestamp) -> element.getTimestamp() * 1000L))
+                // 乱序数据设置时间戳和watermark(使用固定延时策略生成水印)
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<SensorReading>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<SensorReading>() {
+                            @Override
+                            public long extractTimestamp(SensorReading element, long recordTimestamp) {
+                                return element.getTimestamp() * 1000L;
+                            }
+                        }));
 
         OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("late") {
         };
 
-        // 基于事件时间的开窗聚合，统计15秒内温度的最小值
-        SingleOutputStreamOperator<SensorReading> minTempStream = dataStream.keyBy("id")
-                .timeWindow(Time.seconds(15))
+        // 基于事件时间的开窗聚合，统计10秒内温度的最小值
+        SingleOutputStreamOperator<SensorReading> minTempStream = dataStream.keyBy(SensorReading::getId)
+                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
                 .allowedLateness(Time.minutes(1))
                 .sideOutputLateData(outputTag)
                 .minBy("temperature");

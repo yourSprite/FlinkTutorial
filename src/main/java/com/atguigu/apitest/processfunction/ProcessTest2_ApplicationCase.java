3b0802bf3a9de9122fb8a559ca03e812f107b493
wangyutian <wangyutian@jilinxiangyun.com>
2022-03-28 19:18:30 +0800

    commit

--- src/main/java/com/atguigu/apitest/processfunction/ProcessTest2_ApplicationCase.java
+++ src/main/java/com/atguigu/apitest/processfunction/ProcessTest2_ApplicationCase.java
@@ -1,17 +1,8 @@
-package com.atguigu.apitest.processfunction;/**
- * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved
- * <p>
- * Project: FlinkTutorial
- * Package: com.atguigu.apitest.processfunction
- * Version: 1.0
- * <p>
- * Created by wushengran on 2020/11/11 10:54
- */
+package com.atguigu.apitest.processfunction;
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -19,13 +10,12 @@ import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 
 /**
- * @ClassName: ProcessTest2_ApplicationCase
- * @Description:
- * @Author: wushengran on 2020/11/11 10:54
- * @Version: 1.0
+ * @author wangyutian
+ * @version 1.0
+ * @date 2022/3/28
  */
 public class ProcessTest2_ApplicationCase {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
@@ -39,19 +29,19 @@ public class ProcessTest2_ApplicationCase {
         });
 
         // 测试KeyedProcessFunction，先分组然后自定义处理
-        dataStream.keyBy("id")
-                .process( new TempConsIncreWarning(10) )
+        dataStream.keyBy(SensorReading::getId)
+                .process(new TempConsIncreWarning(10))
                 .print();
 
         env.execute();
     }
 
     // 实现自定义处理函数，检测一段时间内的温度连续上升，输出报警
-    public static class TempConsIncreWarning extends KeyedProcessFunction<Tuple, SensorReading, String>{
+    public static class TempConsIncreWarning extends KeyedProcessFunction<String, SensorReading, String> {
         // 定义私有属性，当前统计的时间间隔
-        private Integer interval;
+        private int interval;
 
-        public TempConsIncreWarning(Integer interval) {
+        public TempConsIncreWarning(int interval) {
             this.interval = interval;
         }
 
@@ -60,44 +50,48 @@ public class ProcessTest2_ApplicationCase {
         private ValueState<Long> timerTsState;
 
         @Override
-        public void open(Configuration parameters) throws Exception {
-            lastTempState = getRuntimeContext().getState(new ValueStateDescriptor<Double>("last-temp", Double.class, Double.MIN_VALUE));
-            timerTsState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("timer-ts", Long.class));
+        public void open(Configuration parameters) {
+            lastTempState = getRuntimeContext().getState(new ValueStateDescriptor<>("last-temp", Double.class));
+            timerTsState = getRuntimeContext().getState(new ValueStateDescriptor<>("timer-ts", Long.class));
         }
 
         @Override
-        public void processElement(SensorReading value, Context ctx, Collector<String> out) throws Exception {
+        public void processElement(SensorReading value, KeyedProcessFunction<String, SensorReading, String>.Context ctx, Collector<String> out) throws Exception {
             // 取出状态
             Double lastTemp = lastTempState.value();
             Long timerTs = timerTsState.value();
 
-            // 如果温度上升并且没有定时器，注册10秒后的定时器，开始等待
-            if( value.getTemperature() > lastTemp && timerTs == null ){
-                // 计算出定时器时间戳
-                Long ts = ctx.timerService().currentProcessingTime() + interval * 1000L;
-                ctx.timerService().registerProcessingTimeTimer(ts);
-                timerTsState.update(ts);
+            // 判断lastTempState中状态不为空
+            if (lastTemp == null) {
+                lastTempState.update(value.getTemperature());
+            } else {
+                if (value.getTemperature() > lastTemp && timerTs == null) {
+                    // 计算时间戳并注册定时器
+                    long ts = ctx.timerService().currentProcessingTime() + interval * 1000L;
+                    ctx.timerService().registerProcessingTimeTimer(ts);
+                    // 更新定时状态
+                    timerTsState.update(ts);
+                } else if (value.getTemperature() <= lastTemp && timerTs != null) {
+                    // 删除定时器
+                    ctx.timerService().deleteProcessingTimeTimer(timerTs);
+                    timerTsState.clear();
+                }
+                // 更新温度状态
+                lastTempState.update(value.getTemperature());
             }
-            // 如果温度下降，那么删除定时器
-            else if( value.getTemperature() < lastTemp && timerTs != null ){
-                ctx.timerService().deleteProcessingTimeTimer(timerTs);
-                timerTsState.clear();
-            }
-
-            // 更新温度状态
-            lastTempState.update(value.getTemperature());
         }
 
         @Override
-        public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+        public void onTimer(long timestamp, KeyedProcessFunction<String, SensorReading, String>.OnTimerContext ctx, Collector<String> out) {
             // 定时器触发，输出报警信息
-            out.collect("传感器" + ctx.getCurrentKey().getField(0) + "温度值连续" + interval + "s上升");
+            out.collect("传感器" + ctx.getCurrentKey() + "温度值连续" + interval + "s上升");
             timerTsState.clear();
         }
 
         @Override
-        public void close() throws Exception {
+        public void close() {
             lastTempState.clear();
+            timerTsState.clear();
         }
     }
 }

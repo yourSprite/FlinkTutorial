c27cbd2453026eba5d547122f7fb5a587857a3c1
wangyutian <wangyutian@jilinxiangyun.com>
2022-03-30 13:05:22 +0800

    commit

--- src/main/java/com/atguigu/apitest/tableapi/TableTest1_Example.java
+++ src/main/java/com/atguigu/apitest/tableapi/TableTest1_Example.java
@@ -1,35 +1,27 @@
-package com.atguigu.apitest.tableapi;/**
- * Copyright (c) 2018-2028 尚硅谷 All Rights Reserved
- * <p>
- * Project: FlinkTutorial
- * Package: com.atguigu.apitest.tableapi
- * Version: 1.0
- * <p>
- * Created by wushengran on 2020/11/13 9:40
- */
+package com.atguigu.apitest.tableapi;
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 
+import static org.apache.flink.table.api.Expressions.$;
+
 /**
- * @ClassName: TableTest1_Example
- * @Description:
- * @Author: wushengran on 2020/11/13 9:40
- * @Version: 1.0
+ * @author wangyutian
+ * @version 1.0
+ * @date 2022/3/30
  */
 public class TableTest1_Example {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         // 1. 读取数据
-        DataStreamSource<String> inputStream = env.readTextFile("D:\\Projects\\BigData\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
+        DataStreamSource<String> inputStream = env.readTextFile("D:\\code\\flink\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
 
         // 2. 转换成POJO
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
@@ -44,8 +36,8 @@ public class TableTest1_Example {
         Table dataTable = tableEnv.fromDataStream(dataStream);
 
         // 5. 调用table API进行转换操作
-        Table resultTable = dataTable.select("id, temperature")
-                .where("id = 'sensor_1'");
+        Table resultTable = dataTable.select($("id"), $("temperature"))
+                .where($("id").isEqual("sensor_1"));
 
         // 6. 执行SQL
         tableEnv.createTemporaryView("sensor", dataTable);
